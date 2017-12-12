a5201cfe94d7fb9f39dde164a751dbf569fc5b2f
Luan Vieira <pereiralv2010@gmail.com>
2017-12-12 02:47:07 -0200

    separando component

---
+++ app/scripts/components/rankingComponent.js
@@ -0,0 +1,119 @@
+import _ from 'lodash';
+import Ajax from '../modules/ajax';
+
+_.templateSettings.variable = "data";
+
+export class RankingComponent {
+	constructor() {
+		this.UI = {
+			$list: document.querySelector('.ranking-list')
+		}
+
+		this.init();
+	}
+
+	getParticipantsTmpl() {
+		return `
+		<% _.forEach(data.participants, function(participant, i){  %>
+			<div class="ranking-item" role="button" tabindex="<%= (i + 2) %>">
+				<div class="ranking-tooltip">
+					<div class="ranking-tooltip-box">
+						<h4 class="ranking-tooltip-title">Gostam</h4>
+						<p class="ranking-tooltip-percent"><%= participant.positive %>%</p>
+					</div>
+
+					<div class="ranking-tooltip-box">
+						<h4 class="ranking-tooltip-title">Não gostam</h4>
+						<p class="ranking-tooltip-percent"><%= participant.negative %>%</p>
+					</div>
+				</div>
+
+				<div class="ranking-image-holder">
+					<div class="ranking-image">
+						<img src="<%= participant.picture %>" alt="<%= participant.name %>">
+					</div>
+
+					<span class="ranking-classification">
+						<%= i+1 %>
+					</span>
+				</div>
+
+				<div class="ranking-infos">
+					<h3 class="ranking-name"><%= participant.name %></h3>
+					<p class="ranking-description"><%= participant.description %></p>
+				</div>
+			</div>
+		<% }) %>
+		`
+	}
+
+	getParticipantsData(cb) {
+		Ajax({
+			url: './fazenda.json',
+			success: (response) => {
+				response = JSON.parse(response)
+				response.data = this.fixData(response.data);
+				response.total = this.getTotalVotes(response.data);
+				response.data = this.calcPercentage(response.data, response.total);
+
+				if(typeof cb == 'function') {
+					cb(response.data)
+				}
+			}
+		})
+	}
+
+	fixData(participants) {
+		return _.map(participants, (participant) => {
+			if(participant.positive && participant.negative) {
+				participant.positive = parseFloat(participant.positive);
+				participant.negative = parseFloat(participant.negative);
+			} else {
+				participant.positive = 0;
+				participant.negative = 0;
+			}
+			return participant;
+		});
+	}
+
+	getTotalVotes(participants) {
+		let total = 0;
+
+		_.each(participants, (participant) => {
+			total += (participant.positive + participant.negative)
+		});
+
+		return total;
+	}
+
+	calcPercentage(participants, total) {
+		return _(participants)
+			.map(participant => {
+				participant.positive = Math.floor((participant.positive * 100) / total);
+				participant.negative = Math.floor((participant.negative * 100) / total);
+				return participant;
+			})
+			.orderBy(['positive'], ['desc']).value();
+	}
+
+	bindEvents() {
+		let btns = this.UI.$list.querySelectorAll('.ranking-item');
+
+		_.forEach(btns, (btn) => {
+			btn.addEventListener('click', (e) => {
+				e.preventDefault();
+				e.currentTarget.focus();
+			})
+		})
+	}
+
+	init() {
+		this.getParticipantsData((participants) =>  {
+			let template = _.template(this.getParticipantsTmpl());
+			let proccessedTmpl = template({participants: participants});
+
+			this.UI.$list.innerHTML = proccessedTmpl;
+			this.bindEvents();
+		})
+	}
+}
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,125 +1,13 @@
 require('../scss/app.scss');
-let _ = require('lodash');
-_.templateSettings.variable = "data";
+import _ from 'lodash';
+import { RankingComponent } from './components/rankingComponent';
 
-let Ajax = require('./modules/ajax');
-
-class RankingComponent {
-	constructor() {
-		this.UI = {
-			$list: document.querySelector('.ranking-list')
-		}
-
-		this.init();
-	}
-
-	getParticipantsTmpl() {
-		return `
-		<% _.forEach(data.participants, function(participant, i){  %>
-			<div class="ranking-item" role="button" tabindex="<%= (i + 2) %>">
-				<div class="ranking-tooltip">
-					<div class="ranking-tooltip-box">
-						<h4 class="ranking-tooltip-title">Gostam</h4>
-						<p class="ranking-tooltip-percent"><%= participant.positive %>%</p>
-					</div>
-
-					<div class="ranking-tooltip-box">
-						<h4 class="ranking-tooltip-title">Não gostam</h4>
-						<p class="ranking-tooltip-percent"><%= participant.negative %>%</p>
-					</div>
-				</div>
-
-				<div class="ranking-image-holder">
-					<div class="ranking-image">
-						<img src="<%= participant.picture %>" alt="<%= participant.name %>">
-					</div>
-
-					<span class="ranking-classification">
-						<%= i+1 %>
-					</span>
-				</div>
-
-				<div class="ranking-infos">
-					<h3 class="ranking-name"><%= participant.name %></h3>
-					<p class="ranking-description"><%= participant.description %></p>
-				</div>
-			</div>
-		<% }) %>
-		`
-	}
-
-	getParticipantsData(cb) {
-		Ajax({
-			url: './fazenda.json',
-			success: (response) => {
-				response = JSON.parse(response)
-				response.data = this.fixData(response.data);
-				response.total = this.getTotalVotes(response.data);
-				response.data = this.calcPercentage(response.data, response.total);
-
-				if(typeof cb == 'function') {
-					cb(response.data)
-				}
-			}
-		})
-	}
-
-	fixData(participants) {
-		return _.map(participants, (participant) => {
-			if(participant.positive && participant.negative) {
-				participant.positive = parseFloat(participant.positive);
-				participant.negative = parseFloat(participant.negative);
-			} else {
-				participant.positive = 0;
-				participant.negative = 0;
-			}
-			return participant;
-		});
-	}
-
-	getTotalVotes(participants) {
-		let total = 0;
-
-		_.each(participants, (participant) => {
-			total += (participant.positive + participant.negative)
-		});
-
-		return total;
-	}
-
-	calcPercentage(participants, total) {
-		return _(participants)
-			.map(participant => {
-				participant.positive = Math.floor((participant.positive * 100) / total);
-				participant.negative = Math.floor((participant.negative * 100) / total);
-				return participant;
-			})
-			.orderBy(['positive'], ['desc']).value();
-	}
-
-	bindEvents() {
-		let btns = this.UI.$list.querySelectorAll('.ranking-item');
-
-		_.forEach(btns, (btn) => {
-			btn.addEventListener('click', (e) => {
-				e.preventDefault();
-				e.currentTarget.focus();
-			})
-		})
-	}
-
-	init() {
-		this.getParticipantsData((participants) =>  {
-			let template = _.template(this.getParticipantsTmpl());
-			let proccessedTmpl = template({participants: participants});
-
-			this.UI.$list.innerHTML = proccessedTmpl;
-			this.bindEvents();
-		})
-	}
-}
-
+let Components = [
+	RankingComponent
+]
 
 window.addEventListener('load', () => {
-	new RankingComponent()
-}, false)
+	_.map(Components, (Component) => {
+		return new Component();
+	})
+}, false);
